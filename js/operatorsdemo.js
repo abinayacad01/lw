cafb41db0631fe824840feb2da64be7f193ab7ae
abinayacad01 <abinayakumarcad01@gmail.com>
2023-12-29 18:02:16 +0530

    addind all files

---
+++ js/operatorsdemo.js
@@ -0,0 +1,41 @@
+   function operatorsdemo()
+   {
+    var a=10;
+    var b=25;
+    var c;
+    console.log("value of a=10 ");
+    console.log("value of b=25 ");    
+    console.log("********");
+    console.log("addition:");
+    c=a+b;
+    console.log("added value of a and b is " +c);    
+    console.log("********");
+    console.log("subtraction:");
+    c=a-b;
+    console.log("subracted value of a and b is " +c);    
+    console.log("********");
+    console.log("multiplication:");
+    c=a*b;
+    console.log("product value of a and b is " +c);    
+    console.log("********");
+    console.log("division:");
+    c=a/b;
+    console.log("divided value of a and b is " +c);    
+    console.log("********");
+    console.log("reminder:");
+    c=a%b;
+    console.log("reminder value of a and b is " +c);    
+    console.log("********");
+    console.log("exponential:");
+    c=a**b;
+    console.log("exponent value of a and b is " +c);    
+    console.log("********");
+
+
+
+}
+operatorsdemo();
+
+// document.getElementById("fname1").innerhtml=document.getElementById("fname").value;
+// document.getElementById("lname1").innerhtml=document.getElementById("lname").value;
+// document.getElementById("address1").innerhtml=document.getElementById("address").value;
---
+++ js/script.js
@@ -0,0 +1,75 @@
+function myprogram() {
+  var a = 50;
+  var b = 47;
+  var c = 39;
+  console.log(a + b);
+  console.log(a - b+c);
+  console.log("sum of the value is"+ (a + b+c));
+  console.log("product of the value is"+ (a*b*c));
+  console.log("the value of a/b is"+(a/b));
+}
+
+{
+	var a=10;
+	var b=35;
+	var c=20;
+	console.log(" the value is"+ (a*b/c));
+	console.log(a%b);
+	console.log(a**b);
+	console.log(a);
+
+	if((a>5))
+		{
+			console.log("correct");
+        }
+	else
+		{
+			console.log("wrong");
+		}
+if((a+b<5))
+		{
+			console.log("correct");
+        }
+	else
+		{
+			console.log("wrong");
+		}
+if((a=!5))
+		{
+			console.log("correct");
+        }
+	else
+		{
+			console.log("wrong");
+		}	
+if((a>5>c))
+		{
+			console.log("correct");
+        }
+	else
+		{
+			console.log("wrong");
+		}
+
+if((a+b-c<=100))
+		{
+			console.log("correct");
+        }
+	else
+		{
+			console.log("wrong");
+		}		
+
+if((a>b>c))
+		{
+			console.log("correct");
+        }
+	else
+		{
+			console.log("wrong");
+		}
+
+
+
+}
+myprogram();
